7ce6a931a90831cbd5d9d7f1fef87f6b5a716a35
bearnacki <bearnacki@gmail.com>
2025-03-09 14:59:49 +0100

    chore: upload icon asset and next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://lognessmonster.com",
+  generateRobotsTxt: true,
+  exclude: ["/twitter-image.*", "/opengraph-image.*", "/icon.*"],
+};
